08fbb1786a075c31a9ea41b215561aa5c5451fa4
Philippe Suter <philippe.suter@gmail.com>
2011-12-07 15:10:14 +0100

    Flags can be set on fields.

--- src/main/scala/cafebabe/FieldHandler.scala
+++ src/main/scala/cafebabe/FieldHandler.scala
@@ -1,5 +1,9 @@
 package cafebabe
 
+import ClassFileTypes._
+
 class FieldHandler(f: FieldInfo, cp: ConstantPool) {
   private val field: FieldInfo = f
+
+  def setFlags(flags : U2) : Unit = { f.accessFlags = flags }
 }
--- src/main/scala/cafebabe/FieldInfo.scala
+++ src/main/scala/cafebabe/FieldInfo.scala
@@ -2,7 +2,7 @@ package cafebabe
 
 import ClassFileTypes._
 
-case class FieldInfo(accessFlags: U2, nameIndex: U2, descriptorIndex: U2, attributes: Seq[AttributeInfo]) extends Streamable {
+case class FieldInfo(var accessFlags: U2, nameIndex: U2, descriptorIndex: U2, attributes: Seq[AttributeInfo]) extends Streamable {
   override def toStream(stream: ByteStream): ByteStream = {
     stream << accessFlags << nameIndex << descriptorIndex
     stream << attributes.size.asInstanceOf[U2] << attributes
