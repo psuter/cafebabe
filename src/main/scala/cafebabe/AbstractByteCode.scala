f826c66612cd178206da0611affaefccda4a4df4
Pradeep Krishnan <pradeepk@soft-point.com>
2012-04-04 15:23:49 -0700

    Implement InvokeInterface

--- src/main/scala/cafebabe/AbstractByteCode.scala
+++ src/main/scala/cafebabe/AbstractByteCode.scala
@@ -216,11 +216,11 @@ object AbstractByteCodes {
 
   // Method invocations
 
-  // Not implemented yet. We should generate the two extra bytes properly ("count" and 0).
-  /* object InvokeInterface {
+  object InvokeInterface {
     def apply(className: String, methodName: String, methodSig: String): AbstractByteCodeGenerator =
-      invokeMethod(INVOKEINTERFACE, className, methodName, methodSig)
-  } */
+      _ << invokeMethod(INVOKEINTERFACE, className, methodName, methodSig) <<
+           RawByte(methodSignatureArgStackEffect(methodSig) + 1) << RawByte(0)
+  }
 
   object InvokeSpecial {
     def apply(className: String, methodName: String, methodSig: String): AbstractByteCodeGenerator =
