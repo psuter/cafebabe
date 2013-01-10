de0ab17ef2e04b32116a1aaf505f01fca86adc89
Philippe Suter <philippe.suter@gmail.com>
2013-01-10 20:16:30 +0100

    Allows creation of classloaders with given parent.

--- src/main/scala/cafebabe/CafebabeClassLoader.scala
+++ src/main/scala/cafebabe/CafebabeClassLoader.scala
@@ -4,17 +4,28 @@ import scala.collection.mutable.{Map=>MutableMap}
 
 /** A `ClassLoader` with the capability for loading cafebabe
  *  `ClassFile`s directly from memory. */
-class CafebabeClassLoader extends ClassLoader {
-  private val classes : MutableMap[String,Array[Byte]] = MutableMap.empty
+class CafebabeClassLoader(parent : ClassLoader) extends ClassLoader(parent) {
+  def this() {
+    this(ClassLoader.getSystemClassLoader())
+  }
+
+  private val classBytes : MutableMap[String,Array[Byte]] = MutableMap.empty
 
   def register(classFile : ClassFile) {
+    val name = classFile.className
+    if(classBytes.isDefinedAt(name)) {
+      throw new IllegalArgumentException("Cannot define the same class twice (%s).".format(name))
+    }
+
     val byteStream = (new ByteStream) << classFile
-    classes(classFile.className) = byteStream.getBytes
+    classBytes(name) = byteStream.getBytes
   }
 
   override def findClass(name : String) : Class[_] = {
-    classes.get(name) match {
-      case Some(ba) => defineClass(name, ba, 0, ba.length)
+    classBytes.get(name) match {
+      case Some(ba) =>
+        defineClass(name, ba, 0, ba.length)
+
       case None => super.findClass(name)
     }
   }
